f73da9e82d0d11478cb24efe6c7d594fc7eb67de
TolkachevPeter <peter.tolkachev@gmail.com>
2020-10-29 20:29:06 +0300

    add api

--- src/App.vue
+++ src/App.vue
@@ -19,12 +19,15 @@ export default {
   name: 'App',
   data() {
     return{
-      todos: [
-        {id: 1, title: 'Купить хлеб', completed: false},
-        {id: 2, title: 'Купить масло', completed: false},
-        {id: 3, title: 'Купить сахар', completed: false},
-      ]
+      todos: []
     }
+  },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
+  .then(response => response.json())
+  .then(json => {
+    this.todos = json
+  })
   },
   methods: {
     removeTodo(id){
